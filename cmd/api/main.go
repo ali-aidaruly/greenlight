9b3af7bca25741059919a631d7e47187bc6c5b36
ali-bazilov <ali.bazilov@indriver.com>
2022-12-12 20:40:59 +0600

    160 page, Deleting a Movie, chapter 7.5

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
+	"github.com/ali-aidaruly/greenlight/internal/data"
 	_ "github.com/lib/pq"
 	"log"
 	"net/http"
@@ -28,6 +29,7 @@ type config struct {
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
 
 func main() {
@@ -58,6 +60,7 @@ func main() {
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	srv := &http.Server{
