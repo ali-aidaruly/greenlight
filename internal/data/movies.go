96d8a541860b70e5d4531a6d82c315a4d92151d6
ali-bazilov <ali.bazilov@indriver.com>
2023-01-25 14:48:37 +0600

    164 Advanced CRUP Operations

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"github.com/ali-aidaruly/greenlight/internal/validator"
@@ -39,13 +40,17 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 	}
 
 	query := `
-			SELECT id, created_at, title, year, runtime, genres, version 
+			SELECT pg_sleep(10), id, created_at, title, year, runtime, genres, version 
 			FROM movies
 			WHERE id = $1`
 
 	var movie Movie
 
-	err := m.DB.QueryRow(query, id).Scan(
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, query, id).Scan(
+		&[]byte{},
 		&movie.ID,
 		&movie.CreatedAt,
 		&movie.Title,
@@ -71,7 +76,7 @@ func (m MovieModel) Update(movie *Movie) error {
 	query := `
 			UPDATE movies
 			SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1 
-			WHERE id = $5
+			WHERE id = $5 AND version = $6
 			RETURNING version`
 
 	args := []interface{}{
@@ -80,11 +85,45 @@ func (m MovieModel) Update(movie *Movie) error {
 		movie.Runtime,
 		pq.Array(movie.Genres),
 		movie.ID,
+		movie.Version,
 	}
-	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
+
+	err := m.DB.QueryRow(query, args...).Scan(&movie.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (m MovieModel) Delete(id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+
+	query := `
+		DELETE FROM movies 
+	   	WHERE id = $1`
+
+	result, err := m.DB.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+
 	return nil
 }
 
